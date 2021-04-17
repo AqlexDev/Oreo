9594d41a4a7d4dc96f423f0412265a82fff92c75
AqlexDev <42573629+AqlexDev@users.noreply.github.com>
2021-04-17 19:12:36 -0400

    voice synth added, clear, randomizer added, optimized

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const addtopButton = document.getElementById('addtopButton')
 const addmidButton = document.getElementById('addmidButton')
 const addbotbutton = document.getElementById('addmidButton')
 const remButton = document.getElementById('remButton')
+const clearButton = document.getElementById('clearButton')
+const randomButton = document.getElementById('randomButton')
+const footer = document.getElementById('footer')
 const cookie = document.getElementById('cookie')
 cookie.childNodes[0].remove()
 cookie.childNodes[1].remove()
 cookie.childNodes[2].remove()
+cookie.childNodes[3].remove()
 const title = document.getElementById('title')
 const audio = [
     new Audio('https://aqlexdev.github.io/Oreo/sounds/rempop.wav'),
@@ -42,23 +46,23 @@ function addlayer(type) {
     const newLayer = document.createElement('img')
     if (type == 'top')
     {
-        newLayer.src = 'https://aqlexdev.github.io/Oreo/img/oreo-top.png'
+        newLayer.src = 'https://gulkoa.github.io/Oreo/img/oreo-top.png'
         newLayer.classList = 'oreo-top oreo-layer'
-        title.textContent = 'o' + title.textContent
+        addToTitle('o')
         cookieLayers.push('top')
     }
     else if (type == 'mid')
     {
-        newLayer.src = 'https://aqlexdev.github.io/Oreo/img/oreo-middle.png'
+        newLayer.src = 'https://gulkoa.github.io/Oreo/img/oreo-middle.png'
         newLayer.classList = 'oreo-middle oreo-layer'
-        title.textContent = 're' + title.textContent
+        addToTitle('re')
         cookieLayers.push('mid')
     }
     else if (type == 'bot')
     {
-        newLayer.src = 'https://aqlexdev.github.io/Oreo/img/oreo-bottom.png'
+        newLayer.src = 'https://gulkoa.github.io/Oreo/img/oreo-bottom.png'
         newLayer.classList = 'oreo-bottom oreo-layer'
-        title.textContent = 'o' + title.textContent
+        addToTitle('o')
         cookieLayers.push('bot')
     }
 
@@ -68,6 +72,11 @@ function addlayer(type) {
     cookie.insertBefore(newLayer, cookie.childNodes[0]);
 
     lastLayerIndex++
+
+    if (window.scrollY + 10 > window.scrollMaxY)
+    footer.style = 'opacity: 1'
+    else
+    footer.style = 'opacity: 0'
 }
 
 function removeLayer() {
@@ -77,12 +86,75 @@ function removeLayer() {
         cookie.childNodes[0].classList += ' removing'
         setTimeout(() => {
             cookie.childNodes[0].remove()
-        }, 290)
+        }, 250)
         if (cookieLayers.pop() == 'mid')
-            title.textContent = title.textContent.substring(2)
+            remFromTitle(2)
         else
-            title.textContent = title.textContent.substring(1)
+            remFromTitle(1)
             
         lastLayerIndex--
     }
+}
+
+
+
+title.onclick = () => {
+    var utterance = new SpeechSynthesisUtterance(titleText)
+    speechSynthesis.speak(utterance)
+}
+
+randomButton.onclick = () => {
+    clearAll()
+    setTimeout(() => {
+    const amountOfLayers = Math.floor(Math.random() * 5 + 2)
+    const types = ['bot', 'mid', 'top']
+    for (let i = 0; i < amountOfLayers; i++)
+    {
+        let type = Math.floor(Math.random() * 3)
+        addlayer(types[type])
+    }
+    }, 250)
+}
+
+clearButton.onclick = clearAll
+
+function clearAll() {
+    audio[0].play()
+    for (layer of cookie.childNodes) {
+        layer.classList += ' removing'
+    }
+    setTimeout(() => {cookie.innerHTML = ''}, 200)
+    title.textContent = ''
+    titleText = ''
+    cookieLayers = []
+    lastLayerIndex = -1
+}
+
+
+var titleText = 'oreo'
+function addToTitle(toAdd) {
+    titleText = toAdd + titleText
+    if (titleText.length > window.innerWidth / 31)
+    {
+        title.textContent = titleText.substring(0, window.innerWidth / 31) + '...'
+    }
+    else
+        title.textContent = titleText
+}
+
+function remFromTitle(amount) {
+    titleText = titleText.substring(amount)
+    if (titleText.length > window.innerWidth / 31)
+    {
+        title.textContent = titleText.substring(0, window.innerWidth / 31) + '...'
+    }
+    else
+        title.textContent = titleText
+}
+
+window.onscroll = () => {
+    if (window.scrollY + 10 > window.scrollMaxY)
+        footer.style = 'opacity: 1'
+    else
+        footer.style = 'opacity: 0'
 }
